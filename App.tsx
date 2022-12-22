490910dce5a9e691e62a84e69aec20fb3fa7887b
Clement   Ambrose <Clement.A@avasoft.com>
2022-12-22 20:17:40 +0530

    Align app.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import * as React from 'react';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import * as React from 'react';
-import CustomStripeWidgetScreen from './src/screens/CustomStripeWidget.screen';
-import PaymentHomeScreen from './src/screens/PaymentHome.screen';
+
 import CheckoutScreen from './src/screens/Stripe/Checkout.screen';
 import CustomerFormScreen from './src/screens/Stripe/CustomerForm.screen';
+import CustomStripeWidgetScreen from './src/screens/CustomStripeWidget.screen';
 import PaymentScreen from './src/screens/Stripe/Payment.screen';
+
 import StripeWidget from './src/components/stripe/StripeWidget.component';
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +16,11 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="CustomStripeWidgetScreen"
+          component={CustomStripeWidgetScreen}
+          options={{headerShown: false}}
+        />
         <Stack.Screen
           name="CustomerFormScreen"
           component={CustomerFormScreen}
@@ -32,11 +39,6 @@ const App = () => {
         <Stack.Screen
           name="StripeWidget"
           component={StripeWidget}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="StripeCustomWidget"
-          component={CustomStripeWidgetScreen}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
